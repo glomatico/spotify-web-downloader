0414f4d492ec9974c8b399ae1268c1b81c4635a1
R. M <50295204+glomatico@users.noreply.github.com>
2023-09-28 14:29:50 -0300

    create constants.py

---
+++ spotify_aac_downloader/constants.py
@@ -0,0 +1,13 @@
+MP4_TAGS_MAP = {
+    "album": "\xa9alb",
+    "album_artist": "aART",
+    "artist": "\xa9ART",
+    "comment": "\xa9cmt",
+    "compilation": "cpil",
+    "copyright": "cprt",
+    "lyrics": "\xa9lyr",
+    "media_type": "stik",
+    "rating": "rtng",
+    "release_date": "\xa9day",
+    "title": "\xa9nam",
+}
--- spotify_aac_downloader/downloader.py
+++ spotify_aac_downloader/downloader.py
@@ -12,19 +12,7 @@ from mutagen.mp4 import MP4, MP4Cover
 from pywidevine import PSSH, Cdm, Device
 from yt_dlp import YoutubeDL
 
-MP4_TAGS_MAP = {
-    "album": "\xa9alb",
-    "album_artist": "aART",
-    "artist": "\xa9ART",
-    "comment": "\xa9cmt",
-    "compilation": "cpil",
-    "copyright": "cprt",
-    "lyrics": "\xa9lyr",
-    "media_type": "stik",
-    "rating": "rtng",
-    "release_date": "\xa9day",
-    "title": "\xa9nam",
-}
+from .constants import *
 
 
 class Downloader:
