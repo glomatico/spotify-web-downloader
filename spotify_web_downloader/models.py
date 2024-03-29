9dd05f16f67a5740e135e4217f63ec27e9a2a737
alacleaker <50295204+glomatico@users.noreply.github.com>
2024-03-29 14:57:39 -0300

    add VideoStreamInfo

--- spotify_web_downloader/models.py
+++ spotify_web_downloader/models.py
@@ -16,3 +16,16 @@ class UrlInfo:
 @dataclass
 class DownloadQueueItem:
     metadata: dict = None
+
+
+@dataclass
+class VideoStreamInfo:
+    base_url: str = None
+    iniatialization_url_template: str = None
+    segment_url_template: str = None
+    end_time_millis: int = None
+    segment_length: int = None
+    profile_id_video: int = None
+    profile_id_audio: int = None
+    file_type_video: str = None
+    file_type_audio: str = None
