3760dabbb3829fed555643fc5f2302967d33794a
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2024-08-24 16:11:07 -0300

    Create utils.py

---
+++ spotify_web_downloader/utils.py
@@ -0,0 +1,14 @@
+import requests
+
+
+def check_response(response: requests.Response):
+    try:
+        response.raise_for_status()
+    except requests.HTTPError:
+        _raise_response_exception(response)
+
+
+def _raise_response_exception(response: requests.Response):
+    raise Exception(
+        f"Request failed with status code {response.status_code}: {response.text}"
+    )
